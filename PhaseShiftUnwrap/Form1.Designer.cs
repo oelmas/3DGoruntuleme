385c17f6a553972587d057f138d4eab77689f03d
Öner ELMAS <oelmas@gmail.com>
2013-04-10 17:47:09 +0300

    BitBucket a yuklemeden onceki degisiklikler

---
+++ PhaseShiftUnwrap/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PhaseShiftUnwrap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.processTSMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.pbRef = new AForge.Controls.PictureBox();
+            this.pbFlipUp = new AForge.Controls.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRef)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlipUp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processTSMI});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1158, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1158, 639);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.splitContainer1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1150, 610);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pbRef);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pbFlipUp);
+            this.splitContainer1.Size = new System.Drawing.Size(1144, 604);
+            this.splitContainer1.SplitterDistance = 620;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1150, 614);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // processTSMI
+            // 
+            this.processTSMI.Name = "processTSMI";
+            this.processTSMI.Size = new System.Drawing.Size(70, 24);
+            this.processTSMI.Text = "Process";
+            this.processTSMI.Click += new System.EventHandler(this.processTSMI_Click);
+            // 
+            // pbRef
+            // 
+            this.pbRef.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbRef.Image = null;
+            this.pbRef.Location = new System.Drawing.Point(0, 0);
+            this.pbRef.Name = "pbRef";
+            this.pbRef.Size = new System.Drawing.Size(620, 604);
+            this.pbRef.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbRef.TabIndex = 0;
+            this.pbRef.TabStop = false;
+            // 
+            // pbFlipUp
+            // 
+            this.pbFlipUp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFlipUp.Image = null;
+            this.pbFlipUp.Location = new System.Drawing.Point(0, 0);
+            this.pbFlipUp.Name = "pbFlipUp";
+            this.pbFlipUp.Size = new System.Drawing.Size(520, 604);
+            this.pbFlipUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbFlipUp.TabIndex = 0;
+            this.pbFlipUp.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 667);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRef)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFlipUp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripMenuItem processTSMI;
+        private AForge.Controls.PictureBox pbRef;
+        private AForge.Controls.PictureBox pbFlipUp;
+    }
+}
+
---
+++ PhaseShiftUnwrap/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+using Accord.Imaging.Converters;
+using Accord.Math;
+using PhaseShiftUnwrap.Properties;
+
+namespace PhaseShiftUnwrap
+{
+    public partial class Form1 : Form
+    {
+        private readonly Bitmap imgOrg1 = Resources.o1;
+        private readonly Bitmap imgOrg2 = Resources.o2;
+        private readonly Bitmap imgOrg3 = Resources.o3;
+        private readonly Bitmap imgOrg4 = Resources.o4;
+        //private readonly Bitmap imgOrg5 = Resources.cinque_terre_small;
+
+        private Bitmap imgref1 = Resources.f1;
+        private Bitmap imgref2 = Resources.f2;
+        private Bitmap imgref3 = Resources.f3;
+        private Bitmap imgref4 = Resources.f4;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void processTSMI_Click(object sender, EventArgs e)
+        {
+            
+            // resimler ters çevrildi
+            imgOrg1.RotateFlip(RotateFlipType.Rotate180FlipX);
+            imgOrg2.RotateFlip(RotateFlipType.Rotate180FlipX);
+            imgOrg3.RotateFlip(RotateFlipType.Rotate180FlipX);
+            imgOrg4.RotateFlip(RotateFlipType.Rotate180FlipX);
+            imgref1.RotateFlip(RotateFlipType.Rotate180FlipX);
+            imgref2.RotateFlip(RotateFlipType.Rotate180FlipX);
+            imgref3.RotateFlip(RotateFlipType.Rotate180FlipX);
+            imgref4.RotateFlip(RotateFlipType.Rotate180FlipX);
+            
+            var filter = new Grayscale(0.2125, 0.7154, 0.0721);
+
+            Bitmap grayorg1 = filter.Apply(imgOrg1);
+            Bitmap grayorg2 = filter.Apply(imgOrg2);
+            Bitmap grayorg3 = filter.Apply(imgOrg3);
+            Bitmap grayorg4 = filter.Apply(imgOrg4);
+
+            Bitmap grayref1 = filter.Apply(imgref1);
+            Bitmap grayref2 = filter.Apply(imgref2);
+            Bitmap grayref3 = filter.Apply(imgref3);
+            Bitmap grayref4 = filter.Apply(imgref4);
+
+            double[,] matrix1;
+            double[,] matrix2;
+            double[,] matrix3;
+            double[,] matrix4;
+            double[,] matrixA;
+            double[,] matrixB;
+            
+            
+            var conv = new ImageToMatrix(min: 0, max: 255);
+            
+            conv.Convert(grayorg1, out matrix1);
+            conv.Convert(grayorg2, out matrix2);
+            conv.Convert(grayorg3, out matrix3);
+            conv.Convert(grayorg4, out matrix4);
+
+
+            matrixA = Matrix.Subtract(matrix4, matrix2);
+            matrixB = Matrix.Subtract(matrix1, matrix3);
+
+
+
+            //double[,] matrix1;
+            //double[,] matrix2;
+            //conv.Convert(flipUp1, out matrix1);
+            //conv.Convert(flipUp3, out matrix2);
+            //double[,] matrixA = Matrix.Subtract(matrix2, matrix1);
+
+            //flipUp.RotateFlip(RotateFlipType.Rotate180FlipX);
+            //pbFlipUp.Image = flipUp;
+        }
+    }
+}
